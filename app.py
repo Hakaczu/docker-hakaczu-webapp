505643ae9f9a557bf80ac8977abecdb39e756c04
Sebastian Szypulski <sszypulski.kess@gmail.com>
2023-06-28 20:15:10 +0200

    update layout and catch api errors

--- app.py
+++ app.py
@@ -6,14 +6,20 @@ app = Flask(__name__)
 def get_meme():
     url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
+    print(response)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
-    return meme_large, subreddit
+    title = response["title"]
+    author = response["author"]
+    return meme_large, subreddit, title, author
 
 @app.route("/")
 def index():
-    meme_pic, subreddit = get_meme()
-    return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit)
+    try:
+        meme_pic, subreddit, title, author = get_meme()
+        return render_template("meme_index.html", meme_pic=meme_pic, subreddit=subreddit, meme_title=title, author=author)
+    except:
+        return render_template("error.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host="0.0.0.0", port=5050)
